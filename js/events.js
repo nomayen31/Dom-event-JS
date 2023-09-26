afbb13df6ee48317905b6320e711e01df7a5d1ea
nomayen <nomayen.ohin@gmail.com>
2023-09-26 21:21:02 +0600

    onclink and function using bg color change

--- js/events.js
+++ js/events.js
@@ -1,1 +1,22 @@
-console.log('this is separate js file');
+// option two onclick function 
+
+function makeRed(){
+    document.body.style.backgroundColor='red';
+}
+
+//  option three 
+const makeBlueButton = document.getElementById('make_blue');
+makeBlueButton.onclick = makeBlue;
+
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+//option three another
+const purpleButton = document.getElementById('make_purple');
+     purpleButton.onclick = makePurple;
+
+     function makePurple() {
+        document.body.style.backgroundColor = 'purple'
+     }
